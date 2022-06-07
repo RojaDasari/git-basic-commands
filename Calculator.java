4dec543b74eb5eb1d81fe8a6ab8ad3141f17d208
RojaDasari <roja55dasari@gmail.com>
2022-06-07 13:54:19 +0530

    committing the changes after incorporating the review comments

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,8 @@
+/**
+* This class contains the functions related to basic arithmetic operations
+* @since 1.0
+* @version 1.0
+ */
 public class Calculator{
     public int sum(int a,int b){
         return a+b;
